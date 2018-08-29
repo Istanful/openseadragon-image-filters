b20bbd899cd8b3b0ed70cc4223f0a21d07221866
Emric <w.e.w@live.se>
2018-08-29 21:06:32 +0200

    An example on how to apply filters
    This utilizes the image on a 'tile:loaded' event from OpenSeaDragon to
    create an fabricjs image with filters.

--- demo/index.js
+++ demo/index.js
@@ -1,24 +1,57 @@
 (function(f, $) {
-  "use strict";
-
   f.filterBackend = f.initFilterBackend();
-
-  $.Viewer.prototype.setFilters = function() {
-    console.log('abc');
+  $.Viewer.prototype.setFilters = function(filters) {
     this.addHandler("tile-loaded", function(e) {
-      const isLoaded = e.tile.loaded;
-      console.log('tile-loaded', isLoaded);
-      // const callback = e.getCompletionCallback();
+      /*
+       * Create a fabric image from the image on the event.
+       * This will be used to apply the given filters.
+       *
+       */
+      var fabricImage = new f.Image(e.image);
+      fabricImage.applyFilters(filters)
 
-      // setTimeout(function() {
-      //   callback();
-      // });
+      /*
+       * Create a canvas as big as the image. This will be used to
+       * draw the image on.
+       *
+       */
+      var canvas = document.createElement( 'canvas' );
+      canvas.width = e.image.width;
+      canvas.height = e.image.height;
 
-      console.log('things');
-    });
-
-    this.addHandler("tile-drawing", function(e) {
-      console.log("tile-drawing", e);
+      /*
+       * Draw the image on the canvas and override the context2D OpenSeadragon
+       * currently draws. This will render the filter on the tile with our new
+       * filters applied.
+       *
+       */
+      var renderedContext = canvas.getContext('2d');
+      e.tile.context2D = renderedContext;
+      e.tile.context2D.drawImage(fabricImage.getElement(),
+        0, 0, e.image.width, e.image.height);
     });
   };
+
+  var viewer = $({
+    id: "image-viewer",
+    prefixUrl: "https://openseadragon.github.io/openseadragon/images/",
+    tileSources: {
+      crossOriginPolicy: 'Anonymous',
+      ajaxWithCredentials: false,
+      "Image": {
+          "xmlns":    "http://schemas.microsoft.com/deepzoom/2008",
+          "Format":   "jpg",
+          "Overlap":  "2",
+          "TileSize": "256",
+          "Size": {
+              "Height": "9221",
+              "Width":  "7026"
+          },
+          "Url": "https://openseadragon.github.io/example-images/highsmith/highsmith_files/"
+      },
+    }
+  });
+
+  var filter = new f.Image.filters.Grayscale();
+  viewer.setFilters([filter]);
 })(fabric, OpenSeadragon);
